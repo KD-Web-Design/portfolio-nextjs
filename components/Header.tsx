ec5c44ca4d51735e1f8437a4a985034ec0192c32
KD-Web-Design <contact.kd.webdesign@gmail.com>
2024-09-02 12:19:58 +0200

    1er commit

---
+++ components/Header.tsx
@@ -0,0 +1,27 @@
+import React from 'react'
+import { Section } from './Section'
+import { Button, buttonVariants } from './ui/button'
+import GithubIcon from './icons/GithubIcon'
+import Link from 'next/link'
+import { cn } from '@/lib/utils'
+import LinkedInIcon from './icons/LinkedInIcon'
+
+export default function Header() {
+  return (
+    <header className='sticky top-0 py-4'>
+        <Section className='flex'>
+            <h1 className='text-lg font-bold text-primary'>kd-web-design.dev</h1>
+            <div className='flex-1'>
+                <ul className='flex items-center gap-2 justify-end'>
+                    <Link href="https://github.com/KD-Web-Design" target='_blank' className={cn(buttonVariants({variant: "outline"}), "size-6 p-0")}>
+                        <GithubIcon size={24} className='text-foreground' />
+                    </Link>
+                    <Link href="https://www.linkedin.com/in/kd-web-design-64b496315/" target='_blank' className={cn(buttonVariants({variant: "outline"}), "size-6 p-0")}>
+                        <LinkedInIcon size={24} className='text-foreground' />
+                    </Link>
+                </ul>
+            </div>
+        </Section>
+    </header>
+  )
+}
